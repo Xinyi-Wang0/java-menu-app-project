041c2912109cc8b85ca8cbcac74d5853533e8cf7
xinyi <xyw2020@student.ubc.ca>
2023-02-23 15:21:19 -0800

    phase1

---
+++ src/main/ui/MenuApp.java
@@ -0,0 +1,94 @@
+package ui;
+
+import model.Dish;
+import model.Menu;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.Scanner;
+
+public class MenuApp {
+    private Menu menu;
+    private Scanner input;
+
+    // EFFECTS: runs the Menu application
+    public MenuApp() throws FileNotFoundException {
+        menu = new Menu();
+        input = new Scanner(System.in);
+        runMenu();
+    }
+
+    // MODIFIES: this
+    // EFFECTS: processes user input
+    private void runMenu() {
+        String command = "";
+
+        while (true) {
+            displayMenu();
+            command = input.next();
+            input.nextLine();
+
+            if (command.equals("Quit")) {
+                System.out.println("Thank you for viewing");
+                break;
+            } else {
+                processCommand(command);
+            }
+            System.out.println("*----------------------------------------------------------*");
+        }
+    }
+
+    // EFFECTS: displays menu of options to user
+    private void displayMenu() {
+        System.out.println("\n" + "* Today Special (Select dish to get more information)");
+        System.out.println("[1] Add dish");
+        System.out.println("[2] Check in detail");
+        System.out.println("[3] Check menu");
+        System.out.println("[4] Number of dishes");
+        System.out.println("[5] Delete dish");
+        System.out.println("Quit");
+    }
+
+    private void processCommand(String command) {
+        if (command.equals("1")) {
+            createDish();
+        }
+        if (command.equals("2")) {
+            System.out.println("Enter dish name: ");
+            String name = input.nextLine();
+            System.out.println(menu.getDishesDetails(name));
+        }
+        if (command.equals("3")) {
+            System.out.println(menu.viewAllDish());
+        }
+        if (command.equals("4")) {
+            System.out.println("Num: " + menu.size());
+        }
+        if (command.equals("5")) {
+            System.out.println("Enter dish name: ");
+            String name = input.nextLine();
+            System.out.println(menu.deleteDish(name));
+        }
+    }
+
+    private void createDish() {
+        System.out.println("Enter dish name: ");
+        String name = input.nextLine();
+        System.out.println("Enter dish ingredients: ");
+        String ingredients = input.nextLine();
+        System.out.println("Enter the price: ");
+        int customers = input.nextInt();
+        menu.addDish(new Dish(name, ingredients, customers));
+        System.out.println("New dish added!");
+    }
+
+
+    // EFFECTS: prints all the dishes on the menu to the console
+    private void printDishes() {
+        List<Dish> dishes = menu.allDishes();
+
+        for (Dish d : dishes) {
+            System.out.println(d);
+        }
+    }
+}
